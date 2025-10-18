e3f308d5557cdca2a3ebb9ec6bb15845caefda2c
NewStorm <2456601397@qq.com>
2025-10-18 21:52:19 +0800

    fix webpack5 找不到 buffer

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 
 function resolve(dir) {
   return path.join(__dirname, dir)
@@ -30,5 +31,24 @@ module.exports = {
         symbolId: 'icon-[name]'
       })
       .end()
+  },
+  
+  // <-- 新增：解决 Webpack 5 找不到 'buffer' 的问题 -->
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        // 解决 'buffer' 模块找不到的问题
+        "buffer": require.resolve("buffer/"),
+        // "process" 变量由 ProvidePlugin 处理
+        "process": false 
+      }
+    },
+    plugins: [
+      // 解决全局变量 Buffer 和 process 的问题
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+        process: 'process/browser', 
+      }),
+    ],
   }
 };
