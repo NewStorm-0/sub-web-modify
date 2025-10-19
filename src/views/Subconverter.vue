0f7ade1df7c20865c72ead1e9cd3d57cd8ede04a
NewStorm <2456601397@qq.com>
2025-10-19 16:12:46 +0800

    add local backend

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -467,6 +467,7 @@ export default {
           "sub.cm": "https://sub.cm/short",
         },
         customBackend: {
+          "本地后端": "http://127.0.0.1:25500",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
           "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
           "つつ-多地防失联【负载均衡+国内优化】": "https://api.tsutsu.one",
@@ -476,6 +477,7 @@ export default {
           "sub作者&lhie1提供": "https://api.dler.io",
         },
         backendOptions: [
+          {value: "http://127.0.0.1:25500"},
           {value: "https://url.v1.mk"},
           {value: "https://sub.d1.mk"},
           {value: "https://api.tsutsu.one"},
